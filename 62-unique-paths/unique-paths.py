6ea42c7beedb316a7e2b482ffd45722216b3f48b
Siddharth <sba.workspace@gmail.com>
2025-05-19 00:40:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.35%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,11 +1,4 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        #Space complexity O(n) TC-O(mXn)
-        dp = [1] * n  
 
-
-        for _ in range(1, m):
-            for j in range(1, n):
-                dp[j] += dp[j - 1]  # Current cell = above (dp[j]) + left (dp[j-1])
-
-        return dp[-1]
+        return math.comb(m + n - 2, m - 1)
