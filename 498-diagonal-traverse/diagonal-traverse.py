122f202354e4f0b91ef100321803174195dc3d8a
Siddharth <sba.workspace@gmail.com>
2025-05-20 20:19:14 +0530

    Time: 3 ms (97.94%) | Memory: 19.8 MB (61.10%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -3,30 +3,36 @@ from typing import List
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         m, n = len(mat), len(mat[0])
-        result = []
-        
-        for s in range(m + n - 1):
-            # Temporary list to store the current diagonal
-            temp = []
-            
-            # Determine the starting row and column based on the sum s
-            if s < n:
-                row = 0
-                col = s
+
+        if m == 0 or n == 0:
+            return []
+
+        arr = []
+        row = col = 0
+        up = True
+
+        for _ in range(m * n):
+            arr.append(mat[row][col])
+
+            if up:
+                if col == n - 1:
+                    row += 1
+                    up = False
+                elif row == 0:
+                    col += 1
+                    up = False
+                else:
+                    row -= 1
+                    col += 1
             else:
-                row = s - n + 1
-                col = n - 1
-            
-            # Traverse the diagonal
-            while row < m and col >= 0:
-                temp.append(mat[row][col])
-                row += 1
-                col -= 1
-            
-            # If even sum, reverse the collected diagonal
-            if s % 2 == 0:
-                result.extend(temp[::-1])
-            else:
-                result.extend(temp)
-        
-        return result
+                if row == m - 1:
+                    col += 1
+                    up = True
+                elif col == 0:
+                    row += 1
+                    up = True
+                else:
+                    row += 1
+                    col -= 1
+
+        return arr
