ab40a89327f6ce6edeb85a8c4ede3ef8a3c16aba
Siddharth <sba.workspace@gmail.com>
2025-05-27 21:30:16 +0530

    Time: 28 ms (96.82%) | Memory: 17.7 MB (82.32%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,17 +1,17 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        MIN=-2147483648 #-2^31
-        MAX=2147483647  #2^31 - 1
-
-        res=0
-        while x:
-            digit=int(math.fmod(x,10)) #coxz python is dumb?? -1%10=9
-            x=int(x/10)                # same -1 //10 =-1
-
-            if(res>MAX//10 or (res==MAX//10 and digit>= MAX%10)):
-                return 0
-            if(res<MIN//10 or (res==MIN//10 and digit<=MIN%10)):
-                return 0
-            
-            res=(res*10)+digit
-        return res
+        s=0
+        if x>0:
+            while x>0:
+                r=x%10
+                s=s*10+r
+                x=x//10
+        else:
+            x=-x
+            while x>0:
+                r=x%10
+                s=s*10+r
+                x=x//10
+            s=-s
+        if s < -2**31 or s > 2**31 - 1: return 0
+        return s
