89434fa6e152226b7423427d584a7e04c16da3eb
Siddharth <sba.workspace@gmail.com>
2025-05-29 10:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (55.14%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,8 +1,9 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n>0 and (n & (n-1)==0)
         
-        # Any power of two has exactly one bit set in its binary representation. 
-        #8 is 1000 and 16 is 10000. 
-        #If you subtract 1 from such numbers, all bits to the right of the set bit become 1, 
-        # and a bitwise AND will result in zero.
+        if n==0:
+            return False
+        if n==1:
+            return True
+        
+        return (n%2==0) and self.isPowerOfTwo(n//2)
