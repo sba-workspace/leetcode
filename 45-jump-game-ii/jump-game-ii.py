aef5ed302a72ceb1f64ee8891922b9b82d37b819
Siddharth <sba.workspace@gmail.com>
2025-06-01 13:53:39 +0530

    Time: 7 ms (79.55%) | Memory: 18.8 MB (34.93%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -2,21 +2,14 @@ from collections import deque
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        #bruteforce
-        n = len(nums)
-        visited = [False] * n
-        queue = deque([(0, 0)])  # (current_index, jumps)
-
-        while queue:
-            index, jumps = queue.popleft()
-            
-            if index == n - 1:
-                return jumps
-            
-            max_jump = nums[index]
-            for next_index in range(index + 1, min(n, index + max_jump + 1)):
-                if not visited[next_index]:
-                    visited[next_index] = True
-                    queue.append((next_index, jumps + 1))
-
+        jumps=0#(region of jumps):[2,3,1,1,4]
+        n=len(nums)             # _ | __ | __
+        end,far=0,0             # 0   1    2
         
+        for i in range(n-1):
+            far=max(far,i+nums[i])
+
+            if i==end:
+                jumps+=1
+                end=far
+        return jumps
