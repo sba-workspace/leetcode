8ea00039b4f6c02138f17557683eb38e28dff324
Siddharth <sba.workspace@gmail.com>
2025-06-03 11:33:50 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (14.76%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,17 +5,15 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node=None
+        
+        if not head:
+            return None
+        
+        newHead=head
 
-        while head:
-            '''None <- 1 <- 2 <- 3    4 -> 5 -> None
-                                 ^    ^
-                                prev curr
-            '''
-            #delete the link between head and heat.next
-            temp=head.next
-            head.next=node
-            #update the value +1 step ahead and repeat the process till head is None
-            node=head
-            head=temp
-        return node
+        if head.next:
+            newHead=self.reverseList(head.next)
+            head.next.next=head
+        head.next=None
+        
+        return newHead
