064f1ff16a9b3a6c876e3b2190b3ead010a18d7f
Siddharth <sba.workspace@gmail.com>
2025-06-03 11:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (14.76%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -8,9 +8,14 @@ class Solution:
         node=None
 
         while head:
+            '''None <- 1 <- 2 <- 3    4 -> 5 -> None
+                                 ^    ^
+                                prev curr
+            '''
+            #delete the link between head and heat.next
             temp=head.next
             head.next=node
-
+            #update the value +1 step ahead and repeat the process till head is None
             node=head
             head=temp
         return node
