bfede13fa50d0ac74473225cc3d32bffb481fb52
Siddharth <sba.workspace@gmail.com>
2025-06-23 13:38:14 +0530

    Time: 7 ms (57.05%) | Memory: 18.9 MB (70.48%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -4,14 +4,17 @@ class Solution:
         visited=[False]*n
         cnt=0
 
-        def dfs(city):
-            for neighbor in range(n):
-                if isConnected[city][neighbor] and not visited[neighbor]:
-                    visited[neighbor]=True
-                    dfs(neighbor)
+        def bfs(city):
+            q=deque([city])
+            while q:
+                node=q.popleft()
+                for neighbor in range(n):
+                    if isConnected[node][neighbor] and not visited[neighbor]:
+                        visited[neighbor]=True
+                        q.append(neighbor)
         for city in range(n):
             if not visited[city]:
                 visited[city]=True
-                dfs(city)
+                bfs(city)
                 cnt+=1
         return cnt
