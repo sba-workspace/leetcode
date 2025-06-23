77611c3299adfad55ecf5c5b349a371b3928d154
Siddharth <sba.workspace@gmail.com>
2025-06-23 13:53:56 +0530

    Time: 3 ms (89.35%) | Memory: 19.1 MB (43.67%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,20 +1,24 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n=len(isConnected)
-        visited=[False]*n
-        cnt=0
+        parent=list(range(n))
 
-        def bfs(city):
-            q=deque([city])
-            while q:
-                node=q.popleft()
-                for neighbor in range(n):
-                    if isConnected[node][neighbor] and not visited[neighbor]:
-                        visited[neighbor]=True
-                        q.append(neighbor)
-        for city in range(n):
-            if not visited[city]:
-                visited[city]=True
-                bfs(city)
-                cnt+=1
-        return cnt
+        def find(p):
+            root=p
+            while root!=parent[root]:
+                parent[root]=parent[parent[root]]
+                root=parent[root]
+
+            return root
+        def union(u,v):
+            root1,root2=find(u),find(v)
+
+            if root1!=root2:
+                parent[root2]=root1
+
+        for i in range(n):
+            for j in range(i+1,n):
+                if isConnected[i][j]:
+                    union(i,j)
+        provinces = len(set(find(i) for i in range(n)))
+        return provinces
