a2508b6369fceb6a82f5a268276484ef503b1ebf
Siddharth <sba.workspace@gmail.com>
2025-06-25 11:59:12 +0530

    Time: 119 ms (78.40%) | Memory: 20.2 MB (74.97%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m,n=len(mat[0]),len(mat)
+        dist=[[float("inf")]*m for _ in range(n)]
+        q=deque()
+
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j]==0:
+                    dist[i][j]=0
+                    q.append((i,j))
+        
+        directions=[(0,1),(1,0),(0,-1),(-1,0)]
+
+        while q:
+            r,c=q.popleft()
+            for dr,dc in directions:
+                nr,nc=dr+r,dc+c
+                if 0<=nr<n and 0<=nc<m:
+                    if dist[nr][nc]>dist[r][c]+1:
+                        dist[nr][nc]=dist[r][c]+1
+                        q.append((nr,nc))
+        return dist
