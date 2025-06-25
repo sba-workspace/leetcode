df23c9032389e00787af62edacf8bacaf35b3459
Siddharth <sba.workspace@gmail.com>
2025-06-25 12:09:13 +0530

    Time: 649 ms (89.37%) | Memory: 84.8 MB (59.87%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -0,0 +1,22 @@
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        m,n=len(isWater[0]),len(isWater)
+        height=[[-1]*m for _ in range(n)]
+        q=deque()
+
+        for i in range(n):
+            for j in range(m):
+                if isWater[i][j]==1:
+                    height[i][j]=0
+                    q.append((i,j))
+        
+        directions=[(0,1),(1,0),(0,-1),(-1,0)]
+
+        while q:
+            r,c=q.popleft()
+            for dr,dc in directions:
+                nr,nc=dr+r,dc+c
+                if 0<=nr<n and 0<=nc<m and height[nr][nc]==-1:
+                        height[nr][nc]=height[r][c]+1
+                        q.append((nr,nc))
+        return height
