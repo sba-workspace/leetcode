1cddeb8cd61a1fd67103e23915bfe5460e015bab
Siddharth <sba.workspace@gmail.com>
2025-07-06 11:53:08 +0530

    Time: 27 ms (99.48%) | Memory: 20.9 MB (43.02%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,27 +1,43 @@
+from collections import defaultdict, deque
+from typing import List
+
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        
-        nei=collections.defaultdict(list)
+
+        L = len(beginWord)
         wordList.append(beginWord)
+
+        # build pattern to word map
+        nei = defaultdict(list)
         for word in wordList:
-            for j in range(len(word)):
-                pattern=word[:j]+"*"+word[j+1:]
+            for i in range(L):
+                pattern = word[:i] + "*" + word[i+1:]
                 nei[pattern].append(word)
-        visited=set([beginWord])
-        q=deque([beginWord])
-        res=1
-        while q:
-            for _ in range(len(q)):  # Process all nodes at current level
-                word = q.popleft()
-                if word == endWord:
-                    return res
-                for j in range(len(word)):
-                    pattern = word[:j] + "*" + word[j+1:]
+
+        # Bidirectional BFS
+        beginSet = set([beginWord])
+        endSet = set([endWord])
+        visited = set([beginWord, endWord])
+        steps = 1
+
+        while beginSet and endSet:
+            # Always expand smaller frontier for efficiency
+            if len(beginSet) > len(endSet):
+                beginSet, endSet = endSet, beginSet
+
+            nextSet = set()
+            for word in beginSet:
+                for i in range(L):
+                    pattern = word[:i] + "*" + word[i+1:]
                     for neiWord in nei[pattern]:
+                        if neiWord in endSet:
+                            return steps + 1
                         if neiWord not in visited:
                             visited.add(neiWord)
-                            q.append(neiWord)
-            res += 1
+                            nextSet.add(neiWord)
+            beginSet = nextSet
+            steps += 1
+
         return 0
