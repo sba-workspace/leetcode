5d0212bfb495f268cc1e6ba935c6b91f775b8890
Siddharth <sba.workspace@gmail.com>
2025-09-08 22:13:43 +0530

    Time: 63 ms (60.92%) | Memory: 32 MB (41.38%) - LeetSync

---
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
+        languages = [set(l) for l in languages]
+
+        dontspeak = set()
+        for u, v in friendships:
+            u = u-1
+            v = v-1
+            if languages[u] & languages[v]: continue
+            dontspeak.add(u)
+            dontspeak.add(v)
+
+        langcount = Counter()
+        for f in dontspeak:
+            for l in languages[f]:
+                langcount[l] += 1
+
+        return 0 if not dontspeak else len(dontspeak) - max(list(langcount.values()))        
