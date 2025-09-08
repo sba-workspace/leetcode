8c4cc637fdd2350f3c54bab710bab7ae9bbd358d
Siddharth <sba.workspace@gmail.com>
2025-09-08 20:53:29 +0530

    Time: 1657 ms (6.06%) | Memory: 28.8 MB (51.52%) - LeetSync

---
+++ 1771-sell-diminishing-valued-colored-balls/sell-diminishing-valued-colored-balls.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, inventory: List[int], orders: int) -> int:
+        fn = lambda x: sum(max(0, xx - x) for xx in inventory) # balls sold 
+    
+        # last true binary search 
+        lo, hi = 0, 10**9
+        while lo < hi: 
+            mid = lo + hi + 1 >> 1
+            if fn(mid) >= orders: lo = mid
+            else: hi = mid - 1
+        
+        ans = sum((x + lo + 1)*(x - lo)//2 for x in inventory if x > lo)
+        return (ans - (fn(lo) - orders) * (lo + 1)) % 1_000_000_007
